474ed0d62085b5f7029634e2bef05530178a6bf8
KyuSSeo <kyuri2665@gmail.com>
2025-03-20 12:29:29 +0900

    Hello World kr
    안녕! 세계!

--- Gitstudy/Program.cs
+++ Gitstudy/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int length = 5;
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("안녕, 세계!");
+
+            }
         }
     }
 }
